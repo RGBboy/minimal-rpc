f048c210585ef96dc16bde859d4e6ea110afae1a
RGBboy <l-_-l@rgbboy.com>
2014-08-14 17:08:17 +1200

    Added initial functionality with tests;

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
  * Module Dependencies
  */
 
-var MinimalRPC;
+var MinimalRPC,
+    Through2 = require('through2'),
+    multibuffer = require('multibuffer');
 
 /**
  * MinimalRPC
@@ -17,7 +19,50 @@ var MinimalRPC;
  */
 MinimalRPC = function () {
 
-  var self = {};
+  var self,
+      rpcs = {},
+      decode,
+      chunks;
+
+  decode = function (chunk, encoding, next) {
+    var partialId,
+        partialData,
+        fn;
+    // decode and direct chunk
+    if (chunks) {
+      chunk = Buffer.concat([chunks, chunk]);
+    };
+
+    partialId = multibuffer.readPartial(chunk);
+
+    if (partialId && partialId[0] && partialId[1]) {
+      fn = rpcs[partialId[0].toString('base64')];
+      if (!fn) {
+         // error!!!
+         next(new Error('Requested RPC not found.'));
+         return;
+      };
+      partialData = multibuffer.readPartial(partialId[1]);
+    };
+
+    if (partialData && partialData[0]) {
+      fn(partialData[0]);
+      chunks = null;
+      if (partialData[1]) {
+        decode(partialData[1], encoding, next);
+        return;
+      } else {
+        next();
+        return;
+      };
+    };
+
+    chunks = chunk;
+    next();
+
+  };
+
+  self = Through2(decode);
 
   /**
    * .registerRPC
@@ -30,6 +75,8 @@ MinimalRPC = function () {
    * @api public
    */
   self.registerRPC = function (id, fn) {
+    var base64Id = id.toString('base64');
+    rpcs[base64Id] = fn;
   };
 
   /**
@@ -41,6 +88,7 @@ MinimalRPC = function () {
    * @api public
    */
   self.callRPC = function (id, data) {
+    self.push(multibuffer.pack([id, data]))
   };
 
   return self;
--- test/minimal-rpc.spec.js
+++ test/minimal-rpc.spec.js
@@ -61,3 +61,66 @@ test('minimalRPC.callRPC should be a function', function (t) {
   t.equal(typeof rpc.callRPC, 'function');
   teardown(t);
 });
+
+test('minimalRPC.callRPC should call the registered function on the other side', function (t) {
+  var stream1 = RPC(),
+      stream2 = RPC(),
+      data1 = 'ping',
+      data2 = 'pong';
+
+  t.plan(2);
+
+  stream1.pipe(stream2).pipe(stream1);
+
+  stream1.registerRPC(new Buffer('0'), function (data) {
+    t.equal(data.toString('utf-8'), data2); // pong
+  });
+
+  stream2.registerRPC(new Buffer('1'), function (data) {
+    t.equal(data.toString('utf-8'), data1); // ping
+    stream2.callRPC(new Buffer('0'), new Buffer(data2));
+  });
+
+  stream1.callRPC(new Buffer('1'), new Buffer(data1));
+});
+
+test('multiple minimalRPC.callRPC should call the registered function on the other side the same number of times', function (t) {
+  var stream1 = RPC(),
+      stream2 = RPC(),
+      data1 = 'abc',
+      fn = function (data) {
+        t.equal(data.toString('utf-8'), data1);
+      };
+
+  t.plan(3);
+
+  stream1.pipe(stream2);
+
+  stream2.registerRPC(new Buffer('0'), fn);
+
+  stream1.callRPC(new Buffer('0'), new Buffer(data1));
+  process.nextTick(function () {
+    stream1.callRPC(new Buffer('0'), new Buffer(data1));
+    process.nextTick(function () {
+      stream1.callRPC(new Buffer('0'), new Buffer(data1));
+    });
+  });
+  
+});
+
+test('minimalRPC.callRPC should trigger an error on the otherside if a function is not registered', function (t) {
+  var stream1 = RPC(),
+      stream2 = RPC(),
+      data1 = 'ping',
+      data2 = 'pong';
+
+  t.plan(1);
+
+  stream1.pipe(stream2);
+
+  stream2.on('error', function (err) {
+    t.ok(err instanceof Error, 'error is an instance of Error');
+  });
+
+  stream1.callRPC(new Buffer('1'), new Buffer(data1));
+});
